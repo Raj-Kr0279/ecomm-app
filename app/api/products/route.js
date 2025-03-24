343461075ab69e5422593624dcc0e591b86b8e68
rajendra <kumarrajendra8802@gmail.com>
2025-03-24 13:58:57 +0530

    api-url-updated

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -5,6 +5,7 @@ const products = [
   { id: 1, name: 'Product 1', price: 29.99 },
   { id: 2, name: 'Product 2', price: 39.99 },
   { id: 3, name: 'Product 3', price: 19.99 },
+  
 ];
 
 export async function GET() {
