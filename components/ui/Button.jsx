dc0cbcbaa43a77633184d2efbc33e2694e8f592a
rajendra <kumarrajendra8802@gmail.com>
2025-03-31 16:46:43 +0530

    merge-errors-fix

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function Button({ children, className, ...props }) {
     return (
       <button
@@ -8,15 +7,4 @@ export default function Button({ children, className, ...props }) {
         {children}
       </button>
     );
-=======
-export default function Button({ children, className, ...props }) {
-    return (
-      <button
-        className={`px-4 py-2 text-white rounded-full bg-primary hover:bg-hoverButton transition-all ${className}`}
-        {...props}
-      >
-        {children}
-      </button>
-    );
->>>>>>> 2be2ee17621a388e99ad33dfbf24dad95b637047
   }
